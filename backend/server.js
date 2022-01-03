5390ee7a80f8a01be50999caa8335ad2905eec3a
David Essien <david@localhost.localdomain>
2022-01-03 17:10:34 +0100

    heroku test initial

--- backend/server.js
+++ backend/server.js
@@ -10,4 +10,11 @@ app.use('/login', (req,res) => {
     });
 });
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve("backend",  "build", "index.html"));
+    });
+}
+
 app.listen(process.env.PORT || 8080, () => console.log('API is running on http://localhost:8080/'));
